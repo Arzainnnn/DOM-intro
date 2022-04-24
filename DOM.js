34ccb5014c2bc89aa64efdd312bb5dc19d20df60
Arzainnnn <mohammedarzain54@gmail.com>
2022-04-24 10:29:46 +0530

    first commit

---
+++ DOM.js
@@ -0,0 +1,14 @@
+let newtitle = document.title="DOM-Intro";
+console.log(newtitle);
+
+let newh2 = document.getElementById("apple");
+console.log(newh2);
+
+let newp = document.getElementsByClassName("orange");
+console.log(newp);
+
+let newp2 = document.querySelector(".orange");
+console.log(newp2);
+
+let newp3 = document.querySelectorAll(".orange");
+console.log(newp3);
